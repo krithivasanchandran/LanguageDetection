fa87d369f05527bb688ccd064432440286ae4c79
Chandran <kchandran@uh.edu>
2018-01-07 23:47:43 +0530

    v0.1

---
+++ Location_Data/src\main\java/com/XMLParser/LocData/XmlParser.java
@@ -0,0 +1,47 @@
+package com.XMLParser.LocData;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Result;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+public class XmlParser {
+	
+	 public static Document parseXml(String urlString)
+	            throws FileNotFoundException, ParserConfigurationException,
+	            SAXException, IOException, TransformerConfigurationException {
+		 
+			 URL url = new URL(urlString);
+			 URLConnection conn = url.openConnection();
+			 
+	        DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory
+	                .newInstance();
+	        DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+	        Document doc = docBuilder.parse(conn.getInputStream(),"UTF-8");
+	        
+	        DOMSource domSource = new DOMSource(doc);
+	        TransformerFactory tf = TransformerFactory.newInstance();
+	        Transformer transformer = tf.newTransformer();
+
+	        
+	        //String xml = doc.getTextContent();
+	        //xml = xml.trim().replaceFirst("^([\\W]+)<","<");
+	       
+	        return doc;
+	    }
+
+
+}
---
+++ Location_Data/src\main\java/com/locdata/realestate/commercialproperty/scraper/helloworld.java
@@ -0,0 +1,39 @@
+package com.locdata.realestate.commercialproperty.scraper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+	    URL url;
+	    InputStream is = null;
+	    BufferedReader br;
+	    String line;
+
+	    try {
+	        url = new URL("http://www.appareluae.com/");
+	        is = url.openStream();  // throws an IOException
+	        br = new BufferedReader(new InputStreamReader(is));
+
+	        while ((line = br.readLine()) != null) {
+	            System.out.println(line);
+	        }
+	    } catch (MalformedURLException mue) {
+	         mue.printStackTrace();
+	    } catch (IOException ioe) {
+	         ioe.printStackTrace();
+	    } finally {
+	        try {
+	            if (is != null) is.close();
+	        } catch (IOException ioe) {
+	            // nothing to see here
+	        }
+	    }
+	}
+	
+}
